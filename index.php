388b24bc64905e9546e1d34d5bf05a14f91d3701
diegoGlitch <148559124+diegoGlitch@users.noreply.github.com>
2024-02-15 17:26:13 +0700

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Устанавливаем код ответа сервера на 301 Moved Permanently
-header("HTTP/1.1 301 Moved Permanently");
-// Указываем новый адрес для редиректа
-header("Location: https://vavada.icu");
-exit();
-?>
